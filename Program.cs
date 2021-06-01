d5c76037219428c02d65b9a4be3b26bddcc19d29
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2021-06-01 14:11:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@
             string adresse="ffee:ddcc:bbaa:9988:7766:5544:3322:1100";
             byte[] Result=IPTools.StringToIPv6Address(adresse);
             for (int i=0;i<16;i++) Console.WriteLine(Result[i]);
+            Random r=new Random()
+            Computer c1=new Computer(r);
         }
     }
 
